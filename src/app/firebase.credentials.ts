ac65c750041f552c223506ca447edb380b2edf49
Jose Antonio Adame Fernandez <jose.adame@Merida.net>
2018-01-26 13:09:44 +0100

    first commit :-)

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDilSNCjcUQOYiY4MBxSChRSpjVyJK56_w",
+    authDomain: "shoppinglist-94888.firebaseapp.com",
+    databaseURL: "https://shoppinglist-94888.firebaseio.com",
+    projectId: "shoppinglist-94888",
+    storageBucket: "",
+    messagingSenderId: "55641747089"
+}
